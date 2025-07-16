4a8efed15ca636048aa13871993369528ea37074
binmenja <benjamin.riot@free.fr>
2025-07-16 15:10:52 -0400

    update to add option to choose the number of jacobian recalculations

--- run_multiple_modtran_retrievals.m
+++ run_multiple_modtran_retrievals.m
@@ -1,14 +1,19 @@
-function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
+function [] = run_multiple_modtran_retrievals(datestring,n_recalc_jacob)
 
     % Created by Benjamin Riot--Bretecher (2025). 
     addpath('/home/binmenja/direct/matlab/mylib')
     addpath('/home/binmenja/direct/models/modtran6/bin/linux')
     addpath('/home/binmenja/direct/models/modtran6/bin/linux/matlib_modtran')
-    addpath('/home/binmenja/direct/models/LBLRTM/scripts_brb')
-    if nargin < 2
-        n_recalc_jacob = 2; % jacobian recalculation
+    addpath('/lustre03/project/6003571/binmenja/field_campaigns/whafffers/retrievals')
+
+    if ~exist(datestring,'dir')
+        system(strcat("mkdir -p ", datestring));
     end
-        
+
+    if nargin < 2
+        n_recalc_jacob = 2; % default jacobian recalculation
+    end
+
     % Retrieval settings
     have_jacobian_ready = 0; % whether the jacobian is already ready
     fprintf('Have jacobian ready: %d\n', have_jacobian_ready);
@@ -16,17 +21,19 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
     is_q_log = 0; % whether q is already in log scale; 0: no, 1: yes
     dx_wv = 0.1; % 10 percent uncertainty for wv
     dx_t = 0.5; % 0.5K uncertainty for T
-    altitude_toa = 100;
-    
+
+    cloud.qi = []; cloud.ql = []; cloud.z = []; % clear-sky
+
+
     fprintf('Is q in log scale: %d\n', is_q_log);
-    
-    v1 = 0.2;
-    v2 = 2008;
-    resolution = 0.1;
-    zenith = 0;
-    atmos_id = '5';
-    
-    %% === Automatically find corresponding radiosonde and AERI files ===
+    path_modtran = '/home/binmenja/direct/models/modtran6/bin/linux/'; v1 = 520; v2 = 1800; resolution = 0.1; fwhm = resolution*2;
+    path_tape5 = ['/home/binmenja/direct/field_campaigns/whafffers/tape5/', datestring, '/'];
+    if ~exist(path_tape5,'dir')
+        system(["mkdir -p ", path_tape5]);
+    end
+    modroot = ['whafffers_', datestring, '_retrieval'];
+    angle = 180; emis = 0; iLoc = '0'; ts = 0;
+
     % Extract datetime from profile name (datestring input)
     profile_dt = datetime(datestring, 'InputFormat', 'yyyyMMddHHmm', 'TimeZone', 'UTC');
     
@@ -61,26 +68,26 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
     ref_date_launch = datetime('2000-01-01 00:00:00', 'InputFormat', 'yyyy-MM-dd HH:mm:ss', 'TimeZone', 'UTC') + sonde_dt(1); % Convert to datetime
     disp(ref_date_launch)
     disp(['Radiosonde launch time: ', datestr(ref_date_launch)]);
-    
-    % === Find matching AERI file (by date only) ===
-    aeri_dir = '/home/binmenja/direct/field_campaigns/whafffers/aeri_data/gault/';
-    aeri_files = dir(fullfile(aeri_dir, 'atmospheric.physics_aeri_125_MCGILL_gault_*.nc'));
-    aeri_match = '';
-    
-    for k = 1:length(aeri_files)
-        fname = aeri_files(k).name;
-        if contains(fname, datestr(profile_dt, 'yyyymmdd'))
-            aeri_match = fullfile(aeri_dir, fname);
-            break;
-        end
-    end
-    
-    if isempty(aeri_match)
-        error('No matching AERI file found for %s', datestring);
-    else
-        fprintf('Matched AERI file: %s\n', aeri_match);
-    end
-    aeri_file = aeri_match;
+
+     % === Find matching AERI file (by date only) ===
+     aeri_dir = '/home/binmenja/direct/field_campaigns/whafffers/aeri_data/gault/';
+     aeri_files = dir(fullfile(aeri_dir, 'atmospheric.physics_aeri_125_MCGILL_gault_*.nc'));
+     aeri_match = '';
+     
+     for k = 1:length(aeri_files)
+         fname = aeri_files(k).name;
+         if contains(fname, datestr(profile_dt, 'yyyymmdd'))
+             aeri_match = fullfile(aeri_dir, fname);
+             break;
+         end
+     end
+     
+     if isempty(aeri_match)
+         error('No matching AERI file found for %s', datestring);
+     else
+         fprintf('Matched AERI file: %s\n', aeri_match);
+     end
+     aeri_file = aeri_match;
     AERI_MOPD = ncread(aeri_file,'MOPD'); % AERI MOPD
     AERI_fwhm = 1.2067/(2*AERI_MOPD); % AERI FWHM
     AERI_cal_error_full = ncread(aeri_file, 'RADIANCE.SKY_ERROR');
@@ -107,14 +114,14 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
     time_diff_duration = AERI_dates_full - ref_date_launch; 
     time_diff = minutes(time_diff_duration); 
     start_idx = find(time_diff >= -2, 1, 'first'); 
-    end_idx = find(time_diff <= 8, 1, 'last');  
+    end_idx = find(time_diff <= 8, 1, 'last');    
     if isempty(start_idx), start_idx = 1; end
     if isempty(end_idx), end_idx = length(AERI_dates_full); end
     AERI_rad = AERI_rad_full(:, start_idx:end_idx);
     AERI_cal_error = AERI_cal_error_full(:, start_idx:end_idx); % 1-sigma absolute error
     AERI_nesr = AERI_nesr_full(:, start_idx:end_idx); % 1-sigma noise eq. spe. rad.
 
-    output_dir = fullfile('output_retrievals_lblrtm/38levels', datestring);
+    output_dir = fullfile('output_retrievals_modtran/38levels', datestring);
     if ~exist(output_dir, 'dir'); mkdir(output_dir); end
     cd(output_dir);
 
@@ -135,7 +142,6 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
     z_prior = data_priori.z./1000; % km
     t_prior = data_priori.t_all; % K
     q_prior = data_priori.q_all; % g/kg
-    q_prior = convert_gkg_to_ppmv(q_prior, 'H2O'); % convert to ppmv
     x0_t = nanmean(t_prior,2);
     x0_q = nanmean(q_prior,2); 
     nlev = length(z_truth);
@@ -146,16 +152,14 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
     if ~exist(profile_file, 'file')
     error('Profile file not found: %s', profile_file);
     end
-    load(profile_file);    
+    load(profile_file);      
     z_sonde = profile.z(:) + z_truth(1); % km; adjust to same base height!
     p_sonde = profile.p(:); % hPa
     t_sonde = profile.t(:); % K
     q_sonde = profile.q(:); % g/kg
-    q_sonde = convert_gkg_to_ppmv(q_sonde, 'H2O'); % convert to ppmv
     co2_sonde = profile.co2(:); % ppmv
     ch4_sonde = profile.ch4(:); % ppmv
     o3_sonde = profile.o3; % g/kg
-    o3_sonde = convert_gkg_to_ppmv(o3_sonde, 'O3'); % convert to ppmv
 
     t_truth = interp1(z_sonde,t_sonde,z_truth,'linear','extrap');
     q_truth = exp(interp1(z_sonde,log(q_sonde),z_truth,'linear','extrap'));
@@ -242,7 +246,7 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
     clear d
 
     profile_input.co2 = co2; % ppmv
-    profile_input.o3 = o3; % ppmv
+    profile_input.o3 = o3; % g/kg
     profile_input.ch4 = ch4; % ppmv
     profile_input.co = co; % ppmv
     profile_input.z = z_truth; % km
@@ -274,91 +278,92 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
         % Reassign profile_input for q and T
         profile_input.t = tx; % K
         profile_input.q = qx; % g/kg (not log anymore)
+        pwd
+        jacobian_dir = fullfile('.', datestring);
+        if ~exist(jacobian_dir, 'dir')
+            mkdir(jacobian_dir);
+            fprintf('Created directory: %s\n', fullfile(pwd, datestring));
+        end
 
-        jacobian_suffix = sprintf('x%d', i-1);  % e.g., x0, x1, etc.
-        jacobian_path_t = fullfile(['K_t_era5_', jacobian_suffix, '.mat']);
-        jacobian_path_q = fullfile(['K_q_era5_', jacobian_suffix, '.mat']);        
-        jacobian_path_wv = fullfile(['wv_aj_era5_', jacobian_suffix, '.mat']);
+        jacobian_suffix = sprintf('x%d', i-1); % x0 for i=1, x1 for i=2, etc.
+        jacobian_path_t = fullfile('./', datestring, ['K_t_era5_', jacobian_suffix, '.mat']); 
+        jacobian_path_q = fullfile('./', datestring, ['K_q_era5_', jacobian_suffix, '.mat']);
 
         if have_jacobian_ready
-            if exist(jacobian_path_t, 'file') && exist(jacobian_path_q, 'file') && exist(jacobian_path_wv, 'file')
-                K_t = importdata(jacobian_path_t);  
-                K_q = importdata(jacobian_path_q);  
-                wv_aj = importdata(jacobian_path_wv);
+            if exist(jacobian_path_t, 'file') && exist(jacobian_path_q, 'file')
+                load(jacobian_path_t); 
+                K_t_ori = jacobian_info.jacobian .* 1e7;
+                load(jacobian_path_q);
+                K_q_ori = jacobian_info.jacobian .* 1e7;
+                sim_wnum = jacobian_info.wavenumbers;
             else
                 error('Jacobian files not found for iteration %d. Expected: %s and %s', ...
                     i, jacobian_path_t, jacobian_path_q);
             end
         elseif i <= n_recalc_jacob
+            % Update input profile only from i >= 2 onward
             if i > 1
                 profile_input.q = exp(x(nlev+1:end, i)); % g/kg
-                profile_input.t = x(1:nlev, i);          % K
+                profile_input.t = x(1:nlev, i);           % K
             end
 
-            disp(['Computing LBLRTM Jacobians for iteration ', num2str(i), '...']);
-            tic
-            [wv_aj, K_t, K_q] = lblrtm_aeri_aj_clearsky(v1, v2, ...
-                profile_input.z, profile_input.p, profile_input.t, profile_input.t(1), profile_input.q, ...
-                profile_input.co2, profile_input.o3, n2o, co, profile_input.ch4, ...
-                ccl4, f11, f12, zenith, atmos_id, altitude_toa);
-            toc
+            % Compute and save Jacobians
+            compute_modtran_jacobian_temperature(datestring, profile_input, jacobian_path_t);
+            compute_modtran_jacobian_q(datestring, profile_input, jacobian_path_q);
 
-            % Subset and convert to RU
-            [~, index_aj_begin] = min(abs(wv_aj - wv_begin));
-            [~, index_aj_end]   = min(abs(wv_aj - wv_end));
-            wv_aj_sub = wv_aj(index_aj_begin:index_aj_end);
-            if length(wv_aj_sub) ~= length(AERI_wnum_adj) || ...
-            abs(nanmean(wv_aj_sub - AERI_wnum_adj')) > 0.01
-                error('LBLRTM output wavenumbers not aligned with AERI wavenumbers.');
-            end
+            % Load the newly computed jacobians
+            load(jacobian_path_t); % loads variable: jacobian_info
+            K_t_ori = jacobian_info.jacobian .* 1e7; % Units W/(cm^2*sr*cm^{-1})/K
+            load(jacobian_path_q);
+            K_q_ori = jacobian_info.jacobian .* 1e7; % Units W/(cm^2*sr*cm^{-1})/log(g/kg)
+            sim_wnum = jacobian_info.wavenumbers;
 
-            K_t = K_t(index_aj_begin:index_aj_end, :) .* 1e7;  % RU/K
-            K_q = K_q(index_aj_begin:index_aj_end, :) .* 1e7;  % RU/log(ppmv)
-            K_t(isnan(K_t)) = min(abs(K_t(:)));
-            K_q(isnan(K_q)) = min(abs(K_q(:)));
-
-            % Save for reuse
-            save(jacobian_path_t, 'K_t');
-            save(jacobian_path_q, 'K_q');
-            save(jacobian_path_wv, 'wv_aj');
-
+            % After 2nd iteration, stop recomputation
             if i == n_recalc_jacob
                 have_jacobian_ready = 1;
             end
         else
+            % After n_recalc_jacob, reuse last available Jacobian (from x{n-1})
             prev_suffix = sprintf('x%d', n_recalc_jacob - 1);
-            K_t = importdata(fullfile('./', datestring, ['K_t_era5_', prev_suffix, '.mat']));
-            K_q = importdata(fullfile('./', datestring, ['K_q_era5_', prev_suffix, '.mat']));
-            wv_aj = importdata(fullfile('./', datestring, ['wv_aj_era5_', prev_suffix, '.mat']));
+            load(fullfile('./', datestring, ['K_t_era5_', prev_suffix, '.mat']));
+            K_t_ori = jacobian_info.jacobian .* 1e7;
+            load(fullfile('./', datestring, ['K_q_era5_', prev_suffix, '.mat']));
+            K_q_ori = jacobian_info.jacobian .* 1e7;
+            sim_wnum = jacobian_info.wavenumbers;
+        end
+
+        % Convolve to AERI resolution
+        for il = 1:size(K_t_ori,2)
+            K_t(:,il) = band_conv_brb(sim_wnum, K_t_ori(:,il), AERI_wnum_adj, AERI_fwhm, AERI_MOPD, 'Sinc');
+            K_q(:,il) = band_conv_brb(sim_wnum, K_q_ori(:,il), AERI_wnum_adj, AERI_fwhm, AERI_MOPD, 'Sinc');
         end
 
         % FORWARD SIMULATION
         if i == 1
-            disp('Computing LBLRTM forward simulation...');
             tic
-            [~, F] = lblrtm_aeri_clearsky(v1, v2, profile_input.z, profile_input.p, ...
-                profile_input.t, profile_input.t(1), profile_input.q, ...
-                profile_input.co2, profile_input.o3, n2o, co, ...
-                profile_input.ch4, ccl4, f11, f12, zenith, atmos_id, altitude_toa);
-            toc
+            F = run_single_simulation(path_modtran, path_tape5, ...
+                profile_input, modroot, resolution, fwhm, cloud, ...
+                v1, v2, angle, iLoc, emis, profile.z(end), ts, true);
+            F = F.rad_plt .* 1e7; % Convert to RU
         
-            % Convert and subset to AERI grid
-            [~, index_aj_begin] = min(abs(wv_aj - wv_begin));
-            [~, index_aj_end]   = min(abs(wv_aj - wv_end));
-            F = (F(index_aj_begin:index_aj_end))' .* 1e7;
-        
-            % Check for invalid values
+            % Fix negative or NaN values
             bad_idx = isnan(F) | F < 0;
             if any(bad_idx)
                 fprintf('[Iteration %d] Detected NaN or negative radiance values. Fixing...\n', i);
                 F(bad_idx) = eps;
             end
         
-            F_new = F;
+            % Convolve to AERI resolution once
+            F = band_conv_brb(sim_wnum, F, AERI_wnum_adj, AERI_fwhm, AERI_MOPD, 'Sinc');
+            
+            F_new = F; % Cache forward model result
+            toc
         else
-            F = F_new;
+            F = F_new; % Use cached result
         end
-
+        
+        cleanup_modtran_files(path_modtran, modroot); disp('Trying to clean up in current directory as well...');
+        current_path = pwd; current_path = strcat(current_path, '/');cleanup_modtran_files(current_path, modroot);
 
         if strcmp(variablename, 'both')
             K = [K_t K_q]; % dimension: (nwv,nlev x 2)
@@ -367,21 +372,22 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
         else 
             K = K_q; % dimension: (nwv,nlev)
         end
-    
+
         F_output(:,i) = F;
 
         J(i) = (measurement - F)'*inv(Se)*(measurement - F) + (x(:,i) - xa)'*inv(Sa)*(x(:,i) - xa);
 
         J(i+1) = J(i)+1;
+
         while J(i+1)>J(i)
-            disp(['Iteration ', num2str(i)]);
-            disp(['Size of K: ', mat2str(size(K))]);
-            disp(['Size of Se: ', mat2str(size(Se))]);
-            disp(['Size of Sa: ', mat2str(size(Sa))]);
-            disp(['Size of measurement: ', mat2str(size(measurement))]);
-            disp(['Size of F: ', mat2str(size(F))]);
-            disp(['Size of x(:,i): ', mat2str(size(x(:,i)))]);
-            disp(['Size of xa: ', mat2str(size(xa))]);
+            % disp(['Iteration ', num2str(i)]);
+            % disp(['Size of K: ', mat2str(size(K))]);
+            % disp(['Size of Se: ', mat2str(size(Se))]);
+            % disp(['Size of Sa: ', mat2str(size(Sa))]);
+            % disp(['Size of measurement: ', mat2str(size(measurement))]);
+            % disp(['Size of F: ', mat2str(size(F))]);
+            % disp(['Size of x(:,i): ', mat2str(size(x(:,i)))]);
+            % disp(['Size of xa: ', mat2str(size(xa))]);
             
             x(:, i+1) = x(:,i) + inv((1+lambda)*inv(Sa) + K'*inv(Se)*K)*(K'*inv(Se)*(measurement - F)-inv(Sa)*(x(:,i)-xa));
 
@@ -400,16 +406,33 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
             else
                 profile_input.t = tx; % K
                 profile_input.q = qx; % g/kg
-                disp('Computing forward simulation for the next iteration...');
                 tic
-                [~, F_new] = lblrtm_aeri_clearsky(v1,v2,z_truth,p,tx,tx(1),qx,co2,o3,n2o,co,ch4,ccl4,f11,f12,zenith,atmos_id,altitude_toa);
+                F_new = run_single_simulation(path_modtran, path_tape5, ...
+                profile_input, modroot, resolution, fwhm, cloud, ...
+                v1, v2, angle, iLoc, emis, profile.z(end), ts, true);
+                F_new = F_new.rad_plt .* 1e7;
                 toc
-                F_new = (F_new(index_aj_begin:index_aj_end))' .* 1e7;
+                F_new = band_conv_brb(sim_wnum, F_new, AERI_wnum_adj, AERI_fwhm, AERI_MOPD, 'Sinc');
+
+                % Fix negative or NaN values
+                bad_idx = isnan(F) | F < 0;
+                if any(bad_idx)
+                    fprintf('[Iteration %d] Detected NaN or negative radiance values. Fixing...\n', i);
+                    F(bad_idx) = eps;
+                end
                 
                 J(i+1) = (measurement - F_new)'*inv(Se)*(measurement - F_new) + (x(:,i+1) - xa)'*inv(Sa)*(x(:,i+1) - xa);
 
                 fprintf('Forward simulation done for iteration %d.\n', i+1);
 
+                
+                disp('Trying to clean up in current directory...');
+                cleanup_modtran_files(path_modtran, modroot);
+                disp('Trying to clean up in current directory as well...');
+                current_path = pwd;
+                current_path = strcat(current_path, '/');
+                cleanup_modtran_files(current_path, modroot);
+            
             
             end
             if isnan(J(i+1))
@@ -443,7 +466,7 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
 
 
         % Fair version, lambda = 0
-        G = (K' * inv(Se) * K + inv(Sa)) \ (K' * inv(Se));
+        G = (K' * inv(Se) * K + inv(Sa)) \ (K' * inv(Se)); 
         Spos_CR = inv(K'*inv(Se)*K+inv(Sa));
         Spos_output_CR(:,:,i) = Spos_CR;
         A_CR = Spos_CR * K' * inv(Se) * K;
@@ -535,8 +558,8 @@ function [] = run_multiple_lblrtm_retrievals(datestring,n_recalc_jacob)
     disp(pwd);
     disp('Saving retrieval results...');
     disp(['Output directory: ', output_dir]);
-    disp(['Output filename: retrieval_results_lblrtm_', variablename, '_', timestamp, '.mat']);
-    output_filename = sprintf('retrieval_results_lblrtm_%s_%s.mat', variablename, timestamp);    
+    disp(['Output filename: retrieval_results_modtran_', variablename, '_', timestamp, '.mat']);
+    output_filename = sprintf('retrieval_results_modtran_%s_%s.mat', variablename, timestamp);    
     save(output_filename, 'retrieval_results', '-v7.3');
     disp(['Saved retrieval results to: ', output_filename]);
     addpath('/home/binmenja/direct/field_campaigns/whafffers/retrievals')
